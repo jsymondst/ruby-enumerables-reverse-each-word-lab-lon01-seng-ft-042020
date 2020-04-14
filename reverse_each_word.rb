255890d9391ecd40ec548b14eb95daf90e1511bf
Jamie Symonds-Tayler <jamie.s-t@outlook.com>
2020-04-14 16:46:45 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+str_arr = ["hi","how", "are", "you"]
+
+puts str_arr
+
+str_arr.each do |item|
+  item.reverse
+end
+
+puts str_arr
+
+def reverse_each_word(string)
+  array = string.split(" ")
+end
