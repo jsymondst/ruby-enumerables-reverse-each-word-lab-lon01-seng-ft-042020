9097118d1526fba28ddb4e1d5e5bd764d4999d83
Jamie Symonds-Tayler <jamie.s-t@outlook.com>
2020-04-14 16:47:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,11 @@ str_arr = ["hi","how", "are", "you"]
 
 puts str_arr
 
-str_arr.each do |item|
+new_arr = str_arr.each do |item|
   item.reverse
 end
 
-puts str_arr
+puts new_arr
 
 def reverse_each_word(string)
   array = string.split(" ")
